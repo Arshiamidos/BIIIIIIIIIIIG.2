092f8c7f17e721fa61879229ab7193856cce502b
ArashMidos <arshiamidos@gmail.com>
2021-02-26 15:37:49 +0330

    remove target folder

--- target/debug/deps/main-627d43b3a40febce.d
+++
@@ -1,5 +0,0 @@
-/Users/macuser/Documents/GitHub/BIIIIIIIIIIIG.2/big2/target/debug/deps/main-627d43b3a40febce: src/bin/main.rs
-
-/Users/macuser/Documents/GitHub/BIIIIIIIIIIIG.2/big2/target/debug/deps/main-627d43b3a40febce.d: src/bin/main.rs
-
-src/bin/main.rs:
--- target/debug/deps/main-f84c2722734baf3d.d
+++
@@ -1,5 +0,0 @@
-/Users/macuser/Documents/GitHub/BIIIIIIIIIIIG.2/big2/target/debug/deps/main-f84c2722734baf3d: src/bin/main.rs
-
-/Users/macuser/Documents/GitHub/BIIIIIIIIIIIG.2/big2/target/debug/deps/main-f84c2722734baf3d.d: src/bin/main.rs
-
-src/bin/main.rs:
